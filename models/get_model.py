7a02359ecd50b433f2d038ff89e5b68fff591074
frhdila13 <154692686+frhdila13@users.noreply.github.com>
2025-12-28 20:26:22 +0800

    Modify get_model to accept num_classes and num_bands
    For custom hyperspectral dataset

--- models/get_model.py
+++ models/get_model.py
@@ -8,36 +8,36 @@ from .gaht import gaht
 from .gscvit import gscvit
 from .morphFormer import morphFormer
 from .caevt import caevt
-def get_model(model_name, dataset_name, patch_size):
+def get_model(model_name, dataset_name, patch_size, num_classes, num_bands):
     if model_name == 'cnn2d':
-        model = cnn2d(dataset=dataset_name)
+        model = gscvit(num_classes=num_classes, num_bands=num_bands, patch_size=patch_size)
 
     elif model_name == 'sprn':
-        model = SPRN(dataset=dataset_name)
+        model = SPRN(num_classes=num_classes, num_bands=num_bands, patch_size=patch_size)
 
     elif model_name == 'cnn3d':
-        model = cnn3d(dataset_name, patch_size)
+        model = gscvit(num_classes=num_classes, num_bands=num_bands, patch_size=patch_size)
 
     elif model_name == 'hybridsn':
-        model = hybridsn(dataset_name, patch_size)
+        model = hybridsn(num_classes=num_classes, num_bands=num_bands, patch_size=patch_size)
 
     elif model_name == 'spectralformer':
-        model = spectralformer(dataset_name, patch_size)
+        model = spectralformer(num_classes=num_classes, num_bands=num_bands, patch_size=patch_size)
 
     elif model_name == 'ssftt':
-        model = ssftt(dataset_name, patch_size)
+        model = ssftt(num_classes=num_classes, num_bands=num_bands, patch_size=patch_size)
 
     elif model_name == 'gaht':
-        model = gaht(dataset_name, patch_size)
+        model = gaht(num_classes=num_classes, num_bands=num_bands, patch_size=patch_size)
 
     elif model_name == 'morphFormer':
         model = morphFormer(16, 80, 10, False, 8)
 
     elif model_name == 'gscvit':
-        model = gscvit(dataset_name)
+        model = gscvit(num_classes=num_classes, num_bands=num_bands, patch_size=patch_size)
 
     elif model_name == 'caevt':
-        model = caevt(dataset_name)
+        model = caevt(num_classes=num_classes, num_bands=num_bands, patch_size=patch_size)
 
     else:
         raise KeyError("{} model is not supported yet".format(model_name))
